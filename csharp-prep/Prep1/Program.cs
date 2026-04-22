00f98302cfc6dc5ea8757ffd98abeb8a5c73f2f3
tylercaffrey04 <caf24002@byui.edu>
2026-04-22 15:01:47 -0600

    First Prep assignment completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName;
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your first name? ");
+        string lastName;
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+        int x = 10;
+
+        if(x == 10)
+        {
+            Console.WriteLine("10");
+        }
+        else if (x == 20)
+        {
+            Console.WriteLine("20");
+
+        }
+        else
+        {
+            Console.WriteLine("LIAR");
+        }
     }
 }
