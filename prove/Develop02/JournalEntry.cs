00650137638251750292db15e14659be8fa41520
tylercaffrey04 <caf24002@byui.edu>
2026-05-13 14:32:47 -0600

    Added initial entry class structure

--- prove/Develop02/JournalEntry.cs
+++ prove/Develop02/JournalEntry.cs
@@ -1,4 +1,22 @@
 class JournalEntry
 {
-    
+    string _date;
+    string _prompt;
+    string _response;
+
+    string[] _prompts = {"How are you feeling today","Who did you talk to today?" };
+
+    public void CreateJournalEntry()
+    {
+        _date = DateTime.Now.ToShortDateString();
+        _prompt = _prompts[0];
+        Console.WriteLine(_prompt);
+        _response = Console.ReadLine();
+    }
+    public void DisplayEntry()
+    {
+        Console.WriteLine($"{_date}, {_prompt} - {_response}");
+    }
+
 }
+
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        JournalEntry journalEntry = new JournalEntry();
+        journalEntry.CreateJournalEntry();
         
     }
 }
